efc39c2c0eb07720d09f272fc7090bc61b979409
CyberKL <lotfyk29@yahoo.com>
2025-03-15 00:54:02 +0200

    fix errors

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  assetsInclude: ["**/*.json", "**/*.jpg", "**/*.png", "**/*.svg"],
+  assetsInclude: ["**/*.jpg", "**/*.png", "**/*.svg"],
 })
